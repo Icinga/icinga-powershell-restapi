7b19a0fba5e2598a7d7529bc28834c31c0922bc4
Lord Hepipud <contact@lordhepipud.de>
2020-03-24 11:13:45 +0100

    Base skeleton for new REST-Api daemon

---
+++ icinga-powershell-restapi.psm1
@@ -0,0 +1,52 @@
+function Start-IcingaWindowsRESTApi()
+{
+    param(
+        [int]$Port = 5668
+    );
+
+    # Our ScriptBlock for the code being executed inside the thread
+    [ScriptBlock]$IcingaRestApiScript = {
+        # Allow us to parse the framework global data to this thread
+        param($IcingaGlobals);
+
+        # Import the framework library components and initialise it
+        # as daemon
+        Use-Icinga -LibOnly -Daemon;
+
+        # Add a synchronized hashtable to the global data background
+        # daemon hashtable to write data to. In addition it will
+        # allow to share data collected from this daemon with others
+        $IcingaGlobals.BackgroundDaemon.Add(
+            'IcingaPowerShellRestApi',
+            [hashtable]::Synchronized(@{})
+        );
+
+        # Map our Icinga globals to a shorter variable
+        $RestDaemon = $IcingaGlobals.BackgroundDaemon.IcingaPowerShellRestApi;
+
+        # This will add another hashtable to our previous
+        # IcingaPowerShellRestApi hashtable to store actual
+        # endpoint configurations for the API
+        $RestDaemon.Add(
+            'RegisteredEndpoints',
+            [hashtable]::Synchronized(@{})
+        );
+
+        # Keep our code excuted as long as the PowerShell service is
+        # being executed. This is required to ensure we will execute
+        # the code frequently instead of only once
+        while ($TRUE) {
+
+        }
+    }
+
+    # Now create a new thread for our ScriptBlock, assign a name and
+    # parse all required arguments to it. Last but not least start it
+    # directly
+    New-IcingaThreadInstance `
+        -Name "Icinga_PowerShell_Module_REST_Api" `
+        -ThreadPool $global:IcingaDaemonData.IcingaThreadPool.BackgroundPool `
+        -ScriptBlock $IcingaRestApiScript `
+        -Arguments @( $global:IcingaDaemonData, 6000 ) `
+        -Start;
+}
